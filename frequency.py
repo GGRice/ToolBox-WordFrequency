3a195803450c0ae5250d8c607d46d058a942ecc9
GGRice <gretchrice4@gmail.com>
2017-03-11 12:16:12 -0500

    Finished toolbox

--- frequency.py
+++ frequency.py
@@ -15,27 +15,33 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    #opens text and strips the beginning introduction to Project Gutenberg
+    #opens text and strips the introduction and ending to Project Gutenberg
     f = open(file_name, 'r')
     lines = f.readlines()
     curr_line = 0
     while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
       curr_line += 1
-    lines = lines[curr_line+1:]
+
+    end_line = 0
+    while lines[end_line].find('End of the Project Gutenberg EBook of The Adventures of Pinocchio') == -1:
+      end_line += 1
+
+    lines = lines[curr_line+1:end_line]
+
+    #converts to lowercase
+    lower = []
+    for st in lines:
+        lower.append(st.lower())
+
+    #removes punctuation
+    s = ''
+    for line in lower:
+        s += ''.join(ch for ch in line if ch.isalpha() or ch == ' ' or ch == '\n')
 
     # splits into list of just words
-    split_lines = [i.split(' ', 1)[0] for i in lines]
+    split_lines = s.split()
 
-    # strips words of unwanted characters and adds to list
-    stripped = []
-    for word in split_lines:
-        word = word.lower().strip(" ")
-        # makes sure list doesn't include empty strings or new line strings
-        if word != '\n' and word != '':
-            #for some reason this won't strip quotation marks
-            stripped.append(word.strip(" !#$%&'\"()*+,-./:;<=>?@[\]^_`{|}~\r\n"))
-
-    return stripped
+    return split_lines
 
 
 def get_top_n_words(word_list, n):
@@ -57,8 +63,16 @@ def get_top_n_words(word_list, n):
     # orders the hist by word frequency
     ordered_by_frequency = sorted(hist, key=hist.get, reverse=True)
 
+     # list of words I want to remove form histList
+    unwanted = ['a', 'an', 'be', 'and', 'are', 'from', 'for', 'the', 'they',
+    'their', "they're", 'then', 'them', 'is', 'if', 'of', 'with', 'to', 'in',
+    'was', 'that', 'as', 'at', 'this', 'so', 'had','on', 'it', 'or']
+
+    # removes unwanted words from listHist, creates new list
+    wanted = [word for word in ordered_by_frequency if word not in unwanted]
+
     #creates a list of top n words
-    top = ordered_by_frequency[:n]
+    top = wanted[:n]
 
     return top
 
